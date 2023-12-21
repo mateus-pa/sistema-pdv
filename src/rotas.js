44a873861188b5f2e6f408adc4c2a06cbd767bd1
mateus-pa <mateuspinheiro1410@hotmail.com>
2023-12-21 19:48:02 -0300

    feat: cria intermediário de validação do corpo da requisição

--- src/rotas.js
+++ src/rotas.js
@@ -5,4 +5,10 @@ const rotas = express();
 const loginController = require('./controladores/login');
 const usuariosController = require('./controladores/usuarios');
 
+const validaCorpoRequisicao = require('./intermediarios/validaCorpoRequisicao');
+
+const loginSchema = require('./validacoes/loginSchema');
+
+rotas.post('/login', validaCorpoRequisicao(loginSchema));
+
 module.exports = rotas;
