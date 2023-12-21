769d816fe17a2101cfd7217b93354df868af06f1
mateus-pa <mateuspinheiro1410@hotmail.com>
2023-12-21 19:54:20 -0300

    fix: retira ssl do knex

--- src/bancodedados/conexao.js
+++ src/bancodedados/conexao.js
@@ -6,7 +6,6 @@ const knex = require('knex')({
         database: process.env.DB_NAME,
         password: process.env.DB_PASS,
         port: process.env.DB_PORT,
-        ssl: { rejectUnauthorized: false }
     }
 });
 
