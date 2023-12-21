e9660e4b2a07467ab18b2e6ecb54a700c9383ded
mateus-pa <mateuspinheiro1410@hotmail.com>
2023-12-21 20:00:21 -0300

    feat: cria intermediário de autenticação do usuário logado

---
+++ src/intermediarios/autenticacao.js
@@ -0,0 +1,37 @@
+const knex = require('../bancodedados/conexao');
+const jwt = require('jsonwebtoken');
+
+const autenticaUsuario = async (req, res, next) => {
+    const { authorization } = req.headers;
+
+    try {
+        if (!authorization) {
+            return res.status(401).json({ mensagem: 'Para acessar este recurso, um token de autenticação válido deve ser enviado.' });
+        }
+
+        const token = authorization.split(' ')[1];
+
+        const { id } = jwt.verify(token, process.env.JWT_PASS);
+
+        const usuario = await knex('usuarios').where({ id }).first();
+
+        if (usuario.length === 0) {
+            return res.status(401).json({ mensagem: 'Para acessar este recurso, um token de autenticação válido deve ser enviado.' });
+        }
+
+        const { senha, ...dadosUsuario } = usuario;
+
+        req.usuario = dadosUsuario;
+
+        next();
+    } catch (error) {
+        if (jwt.JsonWebTokenError) {
+            return res.status(401).json({ mensagem: 'Para acessar este recurso, um token de autenticação válido deve ser enviado.' });
+        }
+
+        console.log(error);
+        return res.status(500).json({ mensagem: 'erro interno no servidor' });
+    }
+}
+
+module.exports = autenticaUsuario;
--- src/rotas.js
+++ src/rotas.js
@@ -7,6 +7,7 @@ const usuariosControlador = require('./controladores/usuarios');
 const categoriasControlador = require('./controladores/categorias');
 
 const validaCorpoRequisicao = require('./intermediarios/validaCorpoRequisicao');
+const autenticaUsuario = require('./intermediarios/autenticacao');
 
 const loginSchema = require('./validacoes/loginSchema');
 const usuariosSchema = require('./validacoes/usuariosSchema');
@@ -14,6 +15,8 @@ const usuariosSchema = require('./validacoes/usuariosSchema');
 rotas.post('/usuario', validaCorpoRequisicao(usuariosSchema), usuariosControlador.cadastrar);
 
 rotas.post('/login', validaCorpoRequisicao(loginSchema), loginControlador);
+
+rotas.use(autenticaUsuario);
 
 rotas.get('/categoria', categoriasControlador.listar);
 
