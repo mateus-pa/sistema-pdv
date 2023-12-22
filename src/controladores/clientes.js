d8150cb97793f9f4d6bcb52c45c63696fe934636
mateus-pa <mateuspinheiro1410@hotmail.com>
2023-12-21 22:30:14 -0300

    feat: finaliza rota de clientes

---
+++ src/controladores/clientes.js
@@ -0,0 +1,63 @@
+const knex = require('../bancodedados/conexao');
+
+const clientesControlador = {};
+
+clientesControlador.cadastrar = async function (req, res) {
+    const { nome, email, cpf } = req.body;
+
+    try {
+        // TODO:as intermedário de validação do email
+        const quantidadeClientes = await knex('clientes').where({ email });
+
+        if (quantidadeClientes.length > 0) {
+            return res.status(400).json("O email já existe");
+        }
+
+        const cpfClientes = await knex('clientes').where({ cpf });
+
+        if (cpfClientes.length > 0) {
+            return res.status(400).json("O cpf já existe");
+        }
+
+        const clientes = await knex('clientes').insert({ nome, email, cpf });
+
+        if ( clientes == 0) {
+            return res.status(400).json("O cliente não foi cadastrado.");
+        }
+
+        return res.status(200).json("O cliente foi cadastrado com sucesso!");
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+};
+clientesControlador.detalharPerfil = async (req, res) => {
+    try {
+        const detalharCliente = req.cliente;
+        return res.status(200).json(detalharCliente);
+    } catch (error) {
+        return res.status(401).send()
+    }
+}
+
+clientesControlador.editarPerfil = async (req, res) => {
+    const { nome, email, cpf } = req.body;
+
+    try {
+        const verificarCpf = await knex('clientes').where({ cpf });
+        
+        if (verificarCpf.length > 0) {
+            return res.status(403).json('O email já está cadastrado no sistema');
+        }
+        await knex('clientes')
+            .update({ nome, email, cpf })
+            .where({ id: req.usuario.id });
+        return res.status(204).send();
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Erro interno do servidor' });
+    }
+}
+
+module.exports = clientesControlador;
--- src/rotas.js
+++ src/rotas.js
@@ -7,12 +7,14 @@ const loginControlador = require('./controladores/login');
 const usuariosControlador = require('./controladores/usuarios');
 const categoriasControlador = require('./controladores/categorias');
 const produtosControlador = require('./controladores/produtos');
+const clientesControlador = require('./controladores/clientes');
 
 const validaCorpoRequisicao = require('./intermediarios/validaCorpoRequisicao');
 const autenticaUsuario = require('./intermediarios/autenticacao');
 
 const loginSchema = require('./validacoes/loginSchema');
 const usuariosSchema = require('./validacoes/usuariosSchema');
+const clientesSchema = require('./validacoes/clientesSchema');
 
 
 rotas.post('/usuario', validaCorpoRequisicao(usuariosSchema), usuariosControlador.cadastrar);
@@ -25,8 +27,10 @@ rotas.get('/categoria', categoriasControlador.listar);
 rotas.get('/usuario', usuariosControlador.detalharPerfil);
 rotas.put('/usuario', validaCorpoRequisicao(usuariosSchema), usuariosControlador.editarPerfil);
 
-rotas.post('/produto', produtosControlador.cadastrar)
-rotas.put('/produto/:id', produtosControlador.editar)
+rotas.post('/clientes', validaCorpoRequisicao(clientesSchema), clientesControlador.cadastrar);
+rotas.get('/clientes', clientesControlador.detalharPerfil);
+rotas.put('/clientes', validaCorpoRequisicao(clientesSchema), clientesControlador.editarPerfil);
+
 rotas.get('/produto', produtosControlador.listar);
 rotas.delete('/produto/:id', produtosControlador.excluir);
 
